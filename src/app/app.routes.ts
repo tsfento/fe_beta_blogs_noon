b107d137db757703b40585b5c61d5737d33fc300
Tyler Fenton <tsfento@gmail.com>
2025-07-24 14:55:05 -0500

    Fullstack Part 3

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -13,5 +13,10 @@ export const routes: Routes = [
     path: 'login',
     loadComponent: () => import('./features/auth/login/login.component').then((c) => c.LoginComponent),
     canActivate: [noAuthGuard],
+  },
+  {
+    path: 'blogs/new',
+    loadComponent: () => import('./features/blogs/blogs-new/blogs-new.component').then((c) => c.BlogsNewComponent),
+    canActivate: [authGuard],
   }
 ];
