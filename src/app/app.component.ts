a4d616f5c9cf593bb5b71bb7d00d1fcd072f8aca
Tyler Fenton <tsfento@gmail.com>
2025-08-14 13:25:53 -0500

    Realtime Communication

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { MainLayoutComponent } from './core/components/main-layout/main-layout.component';
+import { MatSnackBar, MatSnackBarModule } from '@angular/material/snack-bar';
+import { NotificationService } from './core/services/notification.service';
 
 @Component({
   selector: 'app-root',
-  imports: [MainLayoutComponent],
+  imports: [MainLayoutComponent, MatSnackBarModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'fe_beta_blogs_noon';
+
+  constructor(private notificationService: NotificationService, private snackBar: MatSnackBar) {}
+
+  ngOnInit(): void {
+    this.notificationService.notification$.subscribe(notifcation => {
+      this.snackBar.open(notifcation, 'Dismiss', {
+        duration: 3000,
+      })
+    })
+  }
 }
--- src/app/core/services/blog.service.ts
+++ src/app/core/services/blog.service.ts
@@ -18,4 +18,12 @@ export class BlogService {
   createBlog(blog: FormData): Observable<Blog> {
     return this.http.post<Blog>(`${environment.apiUrl}/blogs`, blog);
   }
+
+  likeBlog(blogId: number): Observable<any> {
+    return this.http.post(`${environment.apiUrl}/blogs/${blogId}/like`, {});
+  }
+
+  unlikeBlog(blogId: number): Observable<any> {
+    return this.http.delete(`${environment.apiUrl}/blogs/${blogId}/unlike`, {});
+  }
 }
